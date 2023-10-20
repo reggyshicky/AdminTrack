6fb180369c29b3c7c2c0c92fdede832766b1c518
millywanderi <wanderinyambura5@gmail.com>
2023-10-20 15:35:28 +0300

    updated student app modles

--- AdminTrack/Students/models.py
+++ AdminTrack/Students/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
+YEAR_CHOICES = [
+    (1, 'First Year'),
+    (2, 'Second Year'),
+    (3, 'Third Year'),
+	(4, 'Fourth Year'),
+]
+
 # Create your models here.
+class Student(models.Model):
+    firstName = models.CharField(max_length=50)
+    lastName = models.CharField(max_length=50)
+    dateOfBirth = models.DateField()
+    yearOfStudy = models.IntegerField(choices=YEAR_CHOICES)
+    
+    def __str__(self):
+        return f'{self.firstName} {self.lastName}'
