b7e1417a5945ea3320543d9d3561e70b8fdba7f8
millywanderi <wanderinyambura5@gmail.com>
2023-10-26 16:30:32 +0300

    added teachers and students urls

--- AdminTrack/Teacher/urls.py
+++ AdminTrack/Teacher/urls.py
@@ -5,8 +5,9 @@ app_name = "Teacher"
 urlpatterns = [
     #path('login/', views.login, name='login'),
     #path('logout/', views.logout, name='logout'),
-    path('view_teachers', views.view_teachers, name='view_teachers'),
-    path('view_teachers/add/', views.add_teacher, name='add_teacher'),
-    path('view_teachers/edit/<int:id>/', views.edit_teacher, name='edit_teacher'),
-    path('view_teachers/delete/<int:id>/', views.delete_teacher, name='delete_teacher'),
+    path('edit/teacher/<int:pk>', views.modified, name="modified"),
+    # path('edit_teachers/edit/<int:id>/', views.edit_teacher, name='edit_teacher'),
+    path('teachers', views.view_teachers, name='view_teachers'),
+    path('create', views.create, name='create'),
+    path('delete_teachers/delete/<int:id>/', views.delete_teacher, name='delete_teacher'),
 ]
